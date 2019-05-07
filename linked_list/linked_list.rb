2ca46875808980977407dcc573dfc382e3361c1a
sushanth bc <sushanthbc@gmail.com>
2019-05-07 12:56:55 +0530

    added test and remove conditions

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,18 +1,18 @@
-class LinkedList
-  class Node
-    attr_accessor :data, :next  
-    def initialize(data)
-      @data = data
-      @next = nil
-    end
+class Node
+  attr_accessor :data, :next
+  def initialize(data)
+    @data = data
+    @next = nil
   end
-  
-  attr_reader :head, :tail, :length  
+end
+
+class LinkedList
+  attr_reader :head, :tail, :length
 
   def initialize
     @head = nil
     @tail = nil
-    @length = 0 
+    @length = 0
   end
 
   def add_item(data)
@@ -28,8 +28,32 @@ class LinkedList
   rescue StandardError => e
     puts e
   end
- 
-  def remove_item(data)
-  end 
-end
 
+  def remove_item(data)
+    raise "We cannot delete empty linked list" if @head == nil
+    current = prev = @head
+    while current
+      if current.data == data
+        if current.next != nil
+          prev = current
+          current = current.next
+        else
+          prev.next = nil
+        end
+        @length -= 1
+        return "Item is deleted from linked: " + data.to_s
+      end
+      prev = current
+      current = current.next
+    end
+
+    def search_data(data)
+      raise "We cannot delete empty linked list" if @head == nil
+      current = @head
+      while current
+        return "element exists" if current.data == data
+      end
+      "can't find data in linked list"
+    end
+  end
+end
--- linked_list/tc_linked_list.rb
+++ linked_list/tc_linked_list.rb
@@ -6,13 +6,11 @@ class TestLinkedList < Test::Unit::TestCase
     add_item_instance = LinkedList.new
     add_item_instance.add_item(4)
     assert_equal(4, add_item_instance.head.data)
-    #add_item_instance.add_item
   end
 
   def test_remove_item
     remove_item_instance = LinkedList.new
     remove_item_instance.add_item(4)
-    remove_instance.remove_item(4)
-    assert_nil(remove_instance.head)
-  end  
+    assert_includes(remove_item_instance.remove_item(4), "deleted")
+  end
 end
