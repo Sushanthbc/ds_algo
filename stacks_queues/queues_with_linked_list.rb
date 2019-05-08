6e7ab5c96b1b6bcd2a857f3d6d2a3201871c7f39
vijaysraj <vijaysraj006@gmail.com>
2019-05-08 18:04:54 +0530

    queues implimentation

---
+++ stacks_queues/queues_with_linked_list.rb
@@ -0,0 +1,54 @@
+class Node
+  attr_accessor :val, :next
+
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
+
+
+class Queue
+  attr_accessor :start, :end, :length
+
+  def initialize(limit)
+      @start = nil
+      @end = nil
+      @length = 0
+      @limit = limit
+  end
+
+  def enqueue(val)
+    return "Stack limit reached" if @length == @limit
+    queue = Node.new(val)
+    if @start.nil?
+      @start = queue
+      @end = queue
+    else
+      @end.next = queue
+      @end = queue
+    end
+    @length += 1
+  end
+
+  def dequeue
+    unless @start.nil?
+      @start = @start.next
+      @length -= 1
+    end
+
+    @end = nil if @length == 0
+  end
+
+  def peek
+    @start.val
+  end
+
+end
+
+
+queue = Queue.new(2)
+queue.enqueue(10)
+
+
+queue.dequeue
