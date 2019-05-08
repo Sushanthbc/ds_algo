5b02fe7b3525c2e7a5c4790a305dd0f90b7387a2
Smruti Panda <smruti.panda@blr-c02vn0x7hv2d.fundingcircle.global>
2019-05-08 18:08:12 +0530

    Queue class implementation

--- queue/queue.rb
+++ queue/queue.rb
@@ -2,11 +2,37 @@ class Queue
 
   attr_accessor :front, :rear
 
-  def initialize(data)
-    @front = data
-    @rear = data
+  def initialize
+    @front = @rear = 0
+    @queue = []
   end
 
   def enqueue_item(item)
+    unless @queue.empty?
+      @rear += 1
+    end
+    @queue << item
   end
+
+  def dequeue_item
+    if @queue.empty?
+      raise "Queue is empty"
+    else
+      @rear -= 1
+    end
+    @queue
+  end
+
+  def search_item(item)
+    if @queue.empty?
+      raise "Queue is empty"
+    else
+      if @queue.include? item
+        return true 
+      else
+        return false
+      end
+    end
+  end
+
 end
--- queue/tc_queue.rb
+++ queue/tc_queue.rb
@@ -6,14 +6,14 @@ class TestQueue < Test::Unit::TestCase
   def test_add_item_to_queue
     queue = Queue.new
     queue.enqueue_item(8)
-    assert_equal(8, queue.rear)
+    assert_equal([8], queue)
   end
 
   def test_remove_item
     queue = Queue.new
     queue.enqueue_item(8)
     queue.dequeue_item(8)
-    assert_equal(8, queue.rear)
+    assert_equal([], queue)
   end
 
   def test_search_item
@@ -22,7 +22,7 @@ class TestQueue < Test::Unit::TestCase
     array_list.each do |i|
       queue.enqueue_item(i)
     end
-    assert_includes(array_list, queue.search_item(4))
-    assert_includes(array_list, queue.search_item(10))
+    assert_includes(true, queue.search_item(4))
+    assert_includes(false, queue.search_item(10))
   end
 end
