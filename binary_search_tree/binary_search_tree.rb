0d77c5557b04c192bac043aa1353ec6700d84afb
sushanth bc <sushanthbc@gmail.com>
2019-05-08 21:36:15 +0530

    completed the add functionality

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -16,16 +16,21 @@ class BinarySearchTree
     @root = nil
   end
 
-  def add_items(data, current)
-    current = @root
-    if current.nil || current.right == nil || current.left == nil
+  def add_items(data, current, prev=nil, side=nil)
+    if current.nil?
       current = TreeNode.new(data)
+      @root = current if @root.nil?
+      if side == "left" && prev != nil
+        prev.right = current
+      elsif side == "right" && prev != nil
+        prev.left = current
+      end
       return data
     end
-    if current.data > data
-      add_items(data, current.right)
+    if current.data < data
+      add_items(data, current.right, current, "left")
     else
-      add_items(data, current.left)
+      add_items(data, current.left, current, "right")
     end
   end
 end
